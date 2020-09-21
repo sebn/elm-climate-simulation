e778f079d7660d8244d3743ff5d084a13a50d8b6
sebn <sebn@users.noreply.github.com>
2020-09-21 23:15:29 +0200

    zsomme_C

--- test/simclimat.js
+++ test/simclimat.js
@@ -104,6 +104,7 @@ var normalizeSimulationValues = sv => {
             delete sv['temperature_data']['datas'][0]['zphig'];
             delete sv['temperature_data']['datas'][0]['zphig_ancien'];
             delete sv['temperature_data']['datas'][0]['zpuit_oce'];
+            delete sv['temperature_data']['datas'][0]['zsomme_C'];
         }
     }
     delete sv['niveau_calottes_data'];

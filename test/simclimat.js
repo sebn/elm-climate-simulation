15ac5b512bcee36b86757088681230ac231d35ba
sebn <sebn@users.noreply.github.com>
2020-09-26 14:41:08 +0200

    concentrations_coo_data

--- test/simclimat.js
+++ test/simclimat.js
@@ -106,8 +106,6 @@ var normalizeSimulationValues = sv => {
     delete sv['modelPhysicsConstants'];
     delete sv['modelVarsConstants'];
     delete sv['emissions_coo_data'];
-    delete sv['concentrations_coo_data'];
-    // delete sv['temperature_data'];
     if (sv['temperature_data']) {
         // delete sv['temperature_data']['past_datas'];
         // delete sv['temperature_data']['datas'];
